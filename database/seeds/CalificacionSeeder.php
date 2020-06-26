326818a31feab8d1c465a6505e643ea0b6ccc42c
JoaquinTR <joaquintricerri@gmail.com>
2020-06-26 17:43:28 -0300

    mas fixes?

--- database/seeds/CalificacionSeeder.php
+++ database/seeds/CalificacionSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Juego;
 
 class CalificacionSeeder extends Seeder
 {
@@ -11,7 +12,7 @@ class CalificacionSeeder extends Seeder
      */
     public function run()
     {
-        foreach(App\Juego::all() as $juego){
+        foreach(Juego::all() as $juego){
             if($juego->id > 1){ //reservo el id 0 para testeo de calificaciones
                 foreach(range(0,20) as $i){
                     $calif = factory(App\Calificacion::class)->make(['juego_id'=>$juego->id,'descripcion'=>'Calificacion de '.$juego->nombre]);
